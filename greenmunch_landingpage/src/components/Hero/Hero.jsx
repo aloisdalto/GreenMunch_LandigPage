00d09b4b453dc3b03f9245fbab096f2f787c30c0
aloisdalto <aloisdalto@gmail.com>
2025-05-08 15:49:25 -0400

    Hero

--- greenmunch_landingpage/src/components/Hero/Hero.jsx
+++ greenmunch_landingpage/src/components/Hero/Hero.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import './Hero.css';
+import * as Icon from 'react-bootstrap-icons';
+
 
 const Hero = () => {
     return(
         <section className="hero-wrapper">
-            <div className="paddings innerWidth hero-container flexCenter">
-                
+            <div className="paddings innerWidth flexCenter hero-container">
+                {/* Left Side */}
+                <div className="flexColStart hero-left">
+                    <div className="hero-title">
+                        <div className= "orange-circle"/>
+                        <h1>Discover <br/> Most Suitable <br/> Property</h1>
+                    </div>
+                    <div className="flexColStart hero-des">
+                        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+                        <span>culpa commodi exercitationem minus voluptatum debitis.</span>
+                    </div>
+                    <div className="search-bar">
+                        <Icon.GeoAlt color="var(--blue)" size={28} />
+                    </div>
+                </div>
+                {/* Right Side */}
+                <div className="flexCenter hero-right">
+                    <div className="image-container">
+                        <img src="./hero-image.png" alt="Hero Image" />
+                    </div>
+                </div>
             </div>
         </section>
     );
-}; 
+};
+
+export default Hero;
