af53519ce5c25c1df961037bc5a70f58210c49ac
aloisdalto <aloisdalto@gmail.com>
2025-05-09 12:35:05 -0400

    Hero 2

--- greenmunch_landingpage/src/components/Hero/Hero.jsx
+++ greenmunch_landingpage/src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Hero.css';
 import * as Icon from 'react-bootstrap-icons';
-
+import CountUp from 'react-countup';
 
 const Hero = () => {
     return(
@@ -14,11 +14,36 @@ const Hero = () => {
                         <h1>Discover <br/> Most Suitable <br/> Property</h1>
                     </div>
                     <div className="flexColStart hero-des">
-                        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
-                        <span>culpa commodi exercitationem minus voluptatum debitis.</span>
+                        <span className="secondaryText">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+                        <span className="secondaryText">culpa commodi exercitationem minus voluptatum debitis.</span>
                     </div>
-                    <div className="search-bar">
-                        <Icon.GeoAlt color="var(--blue)" size={28} />
+                    <div className="flexCenter search-bar">
+                        <Icon.GeoAlt color="var(--blue)" size={25} />
+                        <input type="text" />
+                        <button className="button">Search</button>
+                    </div>
+                    <div className="flexCenter stats">
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={8800} end={9000} duration={4}/>
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Premiun Products</span>
+                        </div>
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={1950} end={2000} duration={4}/>
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Happy Customers</span>
+                        </div>
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp end={28}/>
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Award Winning</span>
+                        </div>
                     </div>
                 </div>
                 {/* Right Side */}
