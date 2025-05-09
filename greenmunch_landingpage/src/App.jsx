e10bab82034583f846d959a33302f5672242434b
aloisdalto <aloisdalto@gmail.com>
2025-05-09 12:52:37 -0400

    Companies

--- greenmunch_landingpage/src/App.jsx
+++ greenmunch_landingpage/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Companies from "./components/Companies/Companies";
 import './App.css'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <Header />
         <Hero />
       </div>
-        
+      <Companies />
     </div>
   );
 }
