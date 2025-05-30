422de17acd24c2147ab989a45820e8b2f87849be
aloisdalto <aloisdalto@gmail.com>
2025-05-30 10:43:14 -0400

    responsive

--- greenmunch_landingpage/src/components/Header/Header.jsx
+++ greenmunch_landingpage/src/components/Header/Header.jsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import './Header.css';
+import {List} from 'react-bootstrap-icons';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const Header = () => {
+    const [menuOpened, setMenuOpened] = useState(false);
+    const getMenuStyles = (menuOpened) => {
+        if(document.documentElement.clientWidth <= 800){
+            return{right: !menuOpened && "-100%" }
+        }
+    };
+
     return (
         <nav className="h-wrapper">
            <div className="flexCenter paddings innerWidth h-container">
                 <img src="./logo.png" alt="logo" width={100}/>
-                <div className="h-menu">
-                    <a href="">
-                        Residencies
-                    </a>
-                    <a href="">
-                        Our Value
-                    </a>
-                    <a href="">
-                        Contact Us
-                    </a>
-                    <a href="">
-                        Get Started
-                    </a>
-                    <button className="button">
+                <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
+                    <div className="h-menu" style={getMenuStyles(menuOpened)}>
                         <a href="">
-                            Contact
+                            Residencies
                         </a>
-                    </button>
+                        <a href="">
+                            Our Value
+                        </a>
+                        <a href="">
+                            Contact Us
+                        </a>
+                        <a href="">
+                            Get Started
+                        </a>
+                        <button className="button">
+                            <a href="">
+                                Contact
+                            </a>
+                        </button>
+                    </div>   
+                </OutsideClickHandler>
+                
+                <div className="menu-icon" onClick={() => setMenuOpened((prev)=>!prev)}>
+                    <List size={30} />
                 </div>
            </div>
+           
         </nav>
     );
 };
--- greenmunch_landingpage/src/components/Hero/Hero.jsx
+++ greenmunch_landingpage/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Hero.css';
 import * as Icon from 'react-bootstrap-icons';
 import CountUp from 'react-countup';
+import { motion, spring } from 'framer-motion';
 
 const Hero = () => {
     return(
@@ -11,7 +12,12 @@ const Hero = () => {
                 <div className="flexColStart hero-left">
                     <div className="hero-title">
                         <div className= "orange-circle"/>
-                        <h1>Discover <br/> Most Suitable <br/> Property</h1>
+                        <motion.h1 
+                            initial={{y: "2rem" , opacity:0}}
+                            animate={{y: 0, opacity:1}}
+                            transition={{duration:2, type:"spring"}}>
+                            Discover <br/> Most Suitable <br/> Property
+                        </motion.h1>
                     </div>
                     <div className="flexColStart hero-des">
                         <span className="secondaryText">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
@@ -48,9 +54,13 @@ const Hero = () => {
                 </div>
                 {/* Right Side */}
                 <div className="flexCenter hero-right">
-                    <div className="image-container">
-                        <img src="./hero-image.png" alt="Hero Image" />
-                    </div>
+                    <motion.div 
+                    className="image-container"
+                    initial={{x: "7rem" , opacity:0}}
+                    animate={{x: 0, opacity:1}}
+                    transition={{duration:2, type:"spring"}}>
+                        <img src="./hero-image.png" alt="Hero" />
+                    </motion.div>
                 </div>
             </div>
         </section>
